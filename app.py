677f42b80851b612388853be46270239fb43e448
danofoundation <dan.dev947366@gmail.com>
2024-07-20 19:54:54 +0300

    Update app

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
 
+import models
+from db import db
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-app = Flask(__name__)
 
-# Configuration for Flask-Smorest and OpenAPI
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Store REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist"
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-# Initialize the API with the Flask app
-api = Api(app)
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    # Configuration for Flask-Smorest and OpenAPI
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    api = Api(app)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+
+    return app
